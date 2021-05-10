c94add279da03c62c1a8f2b8d62f279cf8368156
YesWeDont <47885665+YesWeDont@users.noreply.github.com>
2021-05-10 13:45:50 +1000

    secret

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ document.addEventListener("DOMContentLoaded",()=>{
     function found(e){
         keys++;
         alert(`Pog! You have now found ${keys}/${secrets.length} secrets`);
+        if(keys===secrets.length) alert("Challenge Completed!\n\nDear Client\nYou have been qualified to become a dev.\n Ring +61 0401 483 207 to finish the application process.")
         e.preventDefault()
         e.target.onclick=null
     }
